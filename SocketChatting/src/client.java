ca41b02decf444e87e91da32c211756dfa5fe66a
cheun0327 <cheun0327@gmail.com>
2021-02-13 23:49:33 +0900

    update client

--- SocketChatting/src/client.java
+++ SocketChatting/src/client.java
@@ -5,7 +5,18 @@ public class client {
 
 	public static void main(String[] args) throws IOException{
 		Socket s = new Socket("localhost", 4567);
+		
 
+		PrintWriter pr = new PrintWriter(s.getOutputStream());
+		pr.println("it works!");
+		pr.flush();
+
+
+		InputStreamReader in = new InputStreamReader(s.getInputStream());
+		BufferedReader bf = new BufferedReader(in);
+		
+		String str = br.readLine();
+		System.out.println("server: "+str);
 	}
 
 }
